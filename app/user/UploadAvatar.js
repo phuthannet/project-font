e9fe7f8f6799fbc13a4a6626dedbf86943d3af32
devPhu <2001.pouis@gmail.com>
2024-03-06 01:55:45 +0700

    update user page

--- app/user/UploadAvatar.js
+++ app/user/UploadAvatar.js
@@ -1,131 +1,184 @@
 "use client";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./profile.css";
 import Cookies from "js-cookie";
-import { toast } from "react-toastify";
-import {
-  Form,
-  Button,
-  FormGroup,
-  Input,
-  Label,
-  Modal,
-  ModalBody,
-  ModalFooter,
-  ModalHeader,
-} from "reactstrap";
-const UpoloadAvatar = ({
-  id,
-  username,
-  profile,
-  setisUserUpdated,
-}) => {
-  const [modal, setModal] = useState(false);
-  const [file, setFile] = useState(null);
+import { Upload, Button, Modal, Alert, Space, Spin } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 
-  const toggle = () => {
-    setModal(!modal);
-  };
-
-
-  const handleFileChange = ({ target: { files } }) => {
-    if (files?.length) {
-      const { type } = files[0];
-      if (type === "image/png" || type === "image/jpeg") {
-        setFile(files[0]);
-      } else {
-        toast.error("Accept only png and jpeg image types are allowed*", {
-          hideProgressBar: true,
-        });
-      }
+const UpoloadAvatar = ({ id, username, profile, setisUserUpdated }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [fileList, setFileList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [uploading, setUploading] = useState(false);
+  const handleCancel = () => {
+    if (!uploading) {
+      setModalVisible(false);
     }
   };
 
-  const upateUserAvatarId = async ( profile) => {
-    try {
-      const token = Cookies.get("token");
-      console.log(token);
+  const handleChange = ({ fileList }) => {
+    const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
+    const isFileTypeAllowed = fileList.every((file) =>
+      allowedTypes.includes(file.type)
+    );
 
-      await axios.put(
-        `https://favorable-dawn-95d99e7a24.strapiapp.com/api/users/${id}`,
-        {  profile },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `bearer ${token}`,
-          },
-        }
-      );
-      setisUserUpdated(true);
-    } catch (error) {
-      console.log({ error });
-    }
-  };
-
-  const handleSubmit = async () => {
-    if (!file) {
-      toast.error("File is required*", {
+    if (!isFileTypeAllowed) {
+      toast.error("Only JPEG, PNG, and GIF files are allowed*", {
         hideProgressBar: true,
       });
       return;
     }
 
+    setFileList(fileList);
+  };
+
+  const handleSubmit = async () => {
+    setUploading(true);
+    if (!fileList.length) {
+      setErrorMessage("File is required*");
+      return;
+    }
+
+    const formData = new FormData();
+    fileList.forEach((file) => {
+      formData.append("files", file.originFileObj);
+      formData.append("name", `${username} avatar`);
+    });
+
     try {
-      const files = new FormData();
-      files.append("files", file);
-      files.append("name", `${username} avatar`);
       const token = Cookies.get("token");
-
-
-      const {
-        data: [{ id, profile }],
-      } = await axios.post(`https://favorable-dawn-95d99e7a24.strapiapp.com/api/upload`, files, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          Authorization: `bearer ${token}`,
-        },
-      });
-      upateUserAvatarId(id, profile);
-      setFile(null);
-      setModal(false);
+      const { data } = await axios.post(
+        `https://favorable-dawn-95d99e7a24.strapiapp.com/api/upload`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const { id, profile } = data[0];
+      updateUserAvatarId(id, profile);
+      setFileList([]);
+      setSuccessMessage("Upload Successful");
+      setModalVisible(false);
+      setUploading(false);
     } catch (error) {
-      console.log({ error });
+      console.error(error);
     }
   };
 
+  const updateUserAvatarId = async (profile) => {
+    try {
+      const token = Cookies.get("token");
+      await axios.put(
+        `https://favorable-dawn-95d99e7a24.strapiapp.com/api/users/${id}`,
+        { profile },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setisUserUpdated(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    let timeoutId;
+    if (errorMessage || successMessage) {
+      timeoutId = setTimeout(() => {
+        setErrorMessage(null);
+        setSuccessMessage(null);
+      }, 4000);
+    }
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [errorMessage, successMessage]);
+
   return (
-    <div>
-      <Button size="sm" onClick={toggle}>
-        {`${profile ? "Change" : "Upload"} picture`}
-      </Button>
-      <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle}>{`${
-          profile ? "Change" : "Upload"
-        } your avatar`}</ModalHeader>
-        <ModalBody>
-          <Form>
-            <FormGroup>
-              <Label for="exampleFile">File</Label>
-              <Input
-                type="file"
-                name="file"
-                id="exampleFile"
-                onChange={handleFileChange}
-              />
-            </FormGroup>
-          </Form>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={handleSubmit}>
-            Upload
-          </Button>
-          <Button color="secondary" onClick={toggle}>
-            Cancel
-          </Button>
-        </ModalFooter>
-      </Modal>
-    </div>
+    <>
+      <div>
+        <Button size="sm" onClick={() => setModalVisible(true)}>
+          {`${profile ? "Change" : "Upload"} picture`}
+        </Button>
+        <Modal
+          open={modalVisible}
+          title={`${profile ? "Change" : "Upload"} your profile`}
+          onCancel={handleCancel}
+          footer={[
+            <Button key="cancel" onClick={handleCancel}>
+              Cancel
+            </Button>,
+            <Button
+              key="upload"
+              type="primary"
+              disabled={uploading}
+              onClick={handleSubmit}
+            >
+              Upload
+            </Button>,
+          ]}
+        >
+          {uploading ? (
+            <div className="text-center">
+              <Spin size="large" />
+            </div>
+          ) : (
+            <Upload
+              onChange={handleChange}
+              file={fileList}
+              beforeUpload={() => false}
+              accept=".png,.jpg,.jpeg"
+              multiple={false}
+              maxCount={1}
+            >
+              <Button icon={<UploadOutlined />}>Select File</Button>
+            </Upload>
+          )}
+        </Modal>
+      </div>
+      <div className="fixed top-0 right-0 m-4">
+        {errorMessage && (
+          <Space
+            direction="vertical"
+            style={{
+              width: "100%",
+            }}
+          >
+            <Alert
+              message="Error"
+              description={errorMessage}
+              type="error"
+              showIcon
+              closable
+            />
+          </Space>
+        )}
+        {successMessage && (
+          <Space
+            direction="vertical"
+            style={{
+              width: "100%",
+            }}
+          >
+            <Alert
+              message="Success"
+              description={successMessage}
+              type="success"
+              showIcon
+              closable
+            />
+          </Space>
+        )}
+      </div>
+    </>
   );
 };
 
--- app/user/page.js
+++ app/user/page.js
@@ -1,65 +1,70 @@
 "use client";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import "./profile.css";
-import { UserOutlined } from '@ant-design/icons';
 import Cookies from "js-cookie";
-import UpoloadAvatar from "./UploadAvatar";
-
-import { Avatar, Typography } from 'antd';
+import UploadAvatar from "./UploadAvatar";
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Typography } from "antd";
 
 const { Paragraph } = Typography;
 
-
 const Profile = () => {
   const [user, setUser] = useState({});
-  const [isUserUpdated, setisUserUpdated] = useState(false);
+  const [isUserUpdated, setIsUserUpdated] = useState(false);
   const token = Cookies.get("token");
-
 
   useEffect(() => {
     const getProfileData = async () => {
       console.log(token);
       try {
-        const { data } = await axios.get(`https://favorable-dawn-95d99e7a24.strapiapp.com/api/users/me?populate=*`, {
-          headers: {
-            Authorization: `bearer ${token}`,
-          },
-        });
+        const { data } = await axios.get(
+          `https://favorable-dawn-95d99e7a24.strapiapp.com/api/users/me?populate=*`,
+          {
+            headers: {
+              Authorization: `bearer ${token}`,
+            },
+          }
+        );
         setUser(data);
-        console.log(data)
-        setisUserUpdated(false);
+        console.log(data);
+        setIsUserUpdated(false);
       } catch (error) {
         console.log({ error });
       }
     };
     getProfileData();
-  }, [ isUserUpdated]);
+  }, [isUserUpdated]);
 
   return (
-    <div className="profile">
-      <div className="avatar">
-        <div className="avatar-wrapper">
-          {user.profile ? (
-            <Avatar src={user.profile.url} alt={`${user.username} avatar`} size={130} />
-          ) : (
-            <Avatar icon={<UserOutlined />} />
-          )}
-          <UpoloadAvatar
-            token={token}
-            id={user.id}
-            username={user.username}
-            profile={user.profile}
-            setisUserUpdated={setisUserUpdated}
-          />
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="bg-white shadow-md rounded-lg p-6 mb-6">
+        <div className="flex items-center justify-center">
+          <div className="flex flex-col items-center justify-center">
+            {user.profile ? (
+              <Avatar
+                src={user.profile.url}
+                alt={`${user.username} avatar`}
+                size={130}
+              />
+            ) : (
+              <Avatar icon={<UserOutlined />} size={130} />
+            )}
+            <UploadAvatar
+              token={token}
+              id={user.id}
+              username={user.username}
+              profile={user.profile}
+              setisUserUpdated={setIsUserUpdated}
+            />
+          </div>
         </div>
-      </div>
-      <div className="body">
-        <Paragraph>Name: {user.username}</Paragraph>
-        <Paragraph>Email: {user.email}</Paragraph>
-        <Paragraph>
-          Account created at: {new Date(user.createdAt).toLocaleDateString()}
-        </Paragraph>
+        <div className="mt-4 text-left">
+          <Paragraph className="mb-2">Username: {user.username}</Paragraph>
+          <Paragraph className="mb-2">Email: {user.email}</Paragraph>
+          <Paragraph className="mb-2">
+            Account created at: {new Date(user.createdAt).toLocaleDateString()}
+          </Paragraph>
+        </div>
       </div>
     </div>
   );
