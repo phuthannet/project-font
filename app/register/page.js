1ef0a93a2d0eba167a351bf4f3db1fc443b55eb4
devPhu <2001.pouis@gmail.com>
2024-03-06 07:34:22 +0700

    update icon and wording

--- app/register/page.js
+++ app/register/page.js
@@ -14,8 +14,8 @@ export default function Page() {
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          className="mx-auto h-12 w-auto"
+          src="/favicon.ico"
           alt="Your Company"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
@@ -113,7 +113,7 @@ export default function Page() {
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Submit
+              Sign Up
             </button>
           </div>
         </form>
